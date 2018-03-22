fec1a8150ca46ec73d28c602b31cbbfd309d24b0
Wouter de Winter <wouter.de.winter@artificialindustry.com>
2018-03-22 15:47:26 +0100

    Wip

--- app/server.py
+++ app/server.py
@@ -35,9 +35,11 @@ records = []
 df = pd.DataFrame(columns=['age', 'gender', 'result'])
 df['result'] = df['result'].astype('int')
 
+
 @app.route("/")
 def hello():
     return render_template('index.html')
+
 
 @app.route("/stats")
 def stats():
@@ -49,29 +51,19 @@ def stats():
     age = age.to_dict()
 
     gender = df.groupby("gender").mean()['result'].to_dict()
-    gender = {key: int(value) for key, value in gender.items()}
+    gender = {key: float(value) for key, value in gender.items()}
 
-    # data = {
-    #     "age": age,
-    #     "gender": gender
-    # }
-    #
     data = {
         "age": age,
         "gender": gender
     }
 
-    print(data)
-
-    #return jsonify(df.values)
-    #return df.to_json()
     return jsonify(data)
 
 
 @app.route("/save", methods=['POST'])
 def save():
-    item = request.get_json()
     global df
+    item = request.get_json()
     df = df.append(item, ignore_index=True)
-
     return jsonify({"result": "ok"})
